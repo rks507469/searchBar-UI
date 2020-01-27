3031454ef7f4bad32820af222ad3588aa10a270a
rks507469 <44531949+rks507469@users.noreply.github.com>
2020-01-27 23:50:43 +0530

    First Commit

---
+++ app/src/main/java/com/example/applicationsearchui/SearchPanel.java
@@ -0,0 +1,24 @@
+package com.example.applicationsearchui;
+
+import android.content.Context;
+import android.net.Uri;
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+
+
+public class SearchPanel extends Fragment {
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_search_panel, container, false);
+    }
+
+}
